ecb78bb31755823e1def11d0e41461e39f7df513
Nathlo <thyalan.aurlence@gmail.com>
2024-07-08 11:40:00 -0400

    add relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -20,7 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
+
+  
 
     /**
      * The attributes that should be hidden for serialization.
